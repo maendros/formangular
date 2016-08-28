3dd70f426b344e0f7b83c99aac53f4214ed46ab2
Maendros <Valitsianius@hotmail.com>
2016-08-29 02:58:15 +0300

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+angular.module('firstApp', []).controller('mainController', function() {
+
+		var vm = this;
+
+		vm.message = 'Hey there! Come and see how good I look!';
+
+		vm.computers = [
+				{ name: 'Macbook Pro', color: 'Silver', nerdness: 7 },
+				{ name: 'Yoga 2 Pro', color: 'Gray', nerdness: 6 },
+				{ name: 'Chromebook', color: 'Black', nerdness: 5 }
+		];
+
+		vm.computerData = {};
+		vm.addComputer = function() {
+		// add a computer to the list
+				vm.computers.push({
+				name: vm.computerData.name,
+				color: vm.computerData.color,
+				nerdness: vm.computerData.nerdness
+		});
+		// after our computer has been added, clear the form
+		vm.computerData = {};
+		};
+});
